5437412fcc579e9dc7feba9c921810e12802d92e
gamerjani006 <zolihadur@gmail.com>
2022-05-28 11:25:43 +0200

    added vault opening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import secrets as s
 import os
-import bz2
 import hashlib
 import string
 import base64
@@ -11,7 +10,6 @@ password_alphabet=string.ascii_letters+string.digits+string.punctuation
 
 def generatePass(length):
 	return ''.join([s.choice(password_alphabet) for i in range(length)])
-
 
 def encryptPass(password, keyword):
 	with open('hardware_key.key','rb') as file: #get hardware key
@@ -31,41 +29,60 @@ def encryptPass(password, keyword):
 	for i in range(len(key)): #encrypt password with hardware key
 		password[i % len(password)] ^= key[i]
 
-	'''
-			TODO
-	1. Add keywords 
-	2. Add a settings menu
-	3. Make the database use a master key on top of the hardware key
-	4. Add remove key option
-	5. Reformat database into JSON
-	'''
 	password = base64.b85encode(bytes(password)).decode()
 	with open('database.json', 'r') as f:
 		data = json.load(f)
 		
 		if keyword in data:
 			choice = input('Do you want to overwrite this password? [Y/N] > ')[0].lower()
-			
 			if choice == 'y':
 				pass
 			else:
 				input('\nCancelled, press enter to go back!')
 				os.system('cls||clear')
 				menu()
-				
 		data[keyword] = password
-	return data # compress the key with bzip then encode using base85
+	return True
+
+def openVault():
+	with open('hardware_key.key','rb') as file: #get hardware key
+		key = file.read()
+	with open("database.json", 'r') as f:
+		a=json.load(f)
+
+	temp={}
+	for i in a.keys():
+		keyword = i
+		password = base64.b85decode(a.get(i))
+		password = [i for i in password]
+		for i in range(len(key)): #decrypt password with hardware key
+			password[i % len(password)] ^= key[i]
+		
+		password = ''.join([chr(i) for i in password[64:password.index(2)]])
+		temp[keyword] = password
+	return temp
+
+def bmenu():
+	input('\nPress enter to go back!')
+	os.system('cls||clear')
+	menu()
+
+
+	
 
 def menu():
-	choice = int(input('Choose one:\n\t1. Generate Password\n\t2. Encrypt a password\n\t3. Generate new hardware key\n\t4. Open vault\nChoice > '))
+	choice = int(input('''Choose one:
+	1. Generate Password
+	2. Encrypt a password
+	3. Generate new hardware key
+	4. Open vault
+Choice > '''))
 	
 	if choice == 1:
 		length = int(input('Input password length > '))
 		generated_password = generatePass(length)
 		print('Generated password is:', generated_password)
-		input('\nPress enter to go back!')
-		os.system('cls||clear')
-		menu()
+		bmenu()
 		
 	elif choice == 2:
 		keyword = input('Input your keyword > ')
@@ -78,23 +95,18 @@ def menu():
 			file.write(encrypted_password)
 			
 		print('Key successfully saved!')
-		input('\nPress enter to go back!')
-		os.system('cls||clear')
-		menu()
+		bmenu()
 	
 	elif choice == 3:
 		with open('hardware_key.key','wb') as file:
 			file.write(os.urandom(1024))
 		print('Generated new hardware key!')
-		input('\nPress enter to go back!')
-		os.system('cls||clear')
-		menu()
+		bmenu()
 
 	elif choice == 4:
-		with open('database.json','r') as f:
-			vault=f.read()
-		x=json.loads(vault)
-		print(x)
+		opened_vault = openVault()
+		print(opened_vault)
+		bmenu()
 		
 	else:
 		menu()
